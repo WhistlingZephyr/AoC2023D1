d7869669745b5441613cae3030e8f2760695e1e3
WhistlingZephyr <90854121+WhistlingZephyr@users.noreply.github.com>
2023-12-01 16:46:56 +0530

    fix: change naming

--- benches/all.rs
+++ benches/all.rs
@@ -4,7 +4,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 fn part2bench(c: &mut Criterion) {
     let input = include_str!("../input.txt").trim();
     let mut group = c.benchmark_group("day01.part2");
-    group.bench_function("part1", |b| {
+    group.bench_function("solution", |b| {
         b.iter(|| part2::solve(black_box(input)))
     });
     group.finish();
